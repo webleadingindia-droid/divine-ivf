46266194e9e1fe6efbb3532e2e35cdad4febb254
netramwebsite2026 <netramwebsite2026@gmail.com>
2026-08-29 11:36:45 +0530

    new changes

--- app/icon.tsx
+++ app/icon.tsx
@@ -19,17 +19,7 @@ export default function Icon() {
           borderRadius: "8px",
         }}
       >
-        <svg width="22" height="22" viewBox="0 0 200 200" fill="none">
-          <g stroke="#B83E6B" strokeWidth="9" strokeLinecap="round">
-            <path d="M100 100 C 100 60, 130 40, 150 20" />
-            <path d="M100 100 C 130 90, 155 65, 175 45" />
-            <path d="M100 100 C 140 105, 168 100, 190 92" />
-            <path d="M100 100 C 90 65, 100 35, 95 8" />
-            <path d="M100 100 C 65 90, 45 65, 25 45" />
-            <path d="M100 100 C 60 105, 32 100, 10 92" />
-          </g>
-          <circle cx="100" cy="100" r="14" fill="#B83E6B" />
-        </svg>
+        <img src="https://plain-apac-prod-public.komododecks.com/202608/29/P2uU8uzX7KkJPZkEa4K6/image.jpg" alt="" />
       </div>
     ),
     { ...size }
